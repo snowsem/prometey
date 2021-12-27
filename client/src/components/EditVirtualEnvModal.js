432afe8ed91613163fe6cbf0a2f26190f6b25d78
nizamov timur <tnizamov@spbfiller.com>
2021-12-27 16:20:00 +0300

    tags updating

--- client/src/components/EditVirtualEnvModal.js
+++ client/src/components/EditVirtualEnvModal.js
@@ -1,24 +1,41 @@
-import {Component} from "react";
+import React, {Component} from "react";
 import Modal from "antd/es/modal/Modal";
-import {Input} from "antd";
+import {Layout, Form, Button, notification} from "antd";
+import ServiceInput from "./ServiceInput";
 
 export class EditVirtualEnvModal extends Component {
-
     constructor() {
         super();
         this.state = {
             name: null
         }
     }
-    onChangeName = (e)=>{
-        this.setState((state)=>{
-            return {...state, name: e.target.value}
+
+    onFinish = async (values) => {
+        const payload = Object.entries(values).map(([id, service_github_tag]) => ({ id, service_github_tag }));
+        const url = `http://localhost:8888/virtual_env/${this.props.data?.id}`;
+        const response = await fetch(url, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ virtualEnvServices: payload }),
         });
-    }
+        if (response.status < 400) {
+            notification.success({
+                message: 'Tags are saved',
+            });
+            this.props.closeModalHandler();
+        } else {
+            notification.error({
+                message: 'Oops',
+                description: 'Something went wrong. Try again later',
+            });
+        }
+    };
     componentDidMount() {
-        console.log('mount')
         this.setState((state)=>{
-            return {...state, name: null}
+            return {name: null}
         });
     }
 
@@ -27,14 +44,40 @@ export class EditVirtualEnvModal extends Component {
             return {...state, name: null}
         });
         this.props.closeModalHandler()
-    }
+    };
 
     render() {
+        const initialValues = this.props.data?.virtualEnvServices.reduce((acc, item) => {
+           acc[item.id] = item.service_github_tag;
+           return acc;
+        }, {});
         return (
             <div>
-                <Modal title="sdsad" visible={this.props.visible} onOk={this.onCloseHandler} onCancel={this.onCloseHandler}>
-
-                    <p>Some contents...</p>
+                <Modal
+                    okButtonProps={{form:'tag-editor-form', key: 'submit', htmlType: 'submit'}}
+                    width={1000}
+                    title={this.props.data?.title}
+                    visible={this.props.visible}
+                    onCancel={this.onCloseHandler}
+                >
+                    <Layout style={{ "background-color": "#fff" }}>
+                        <Form
+                            id='tag-editor-form'
+                            onFinish={this.onFinish}
+                            name="basic"
+                            labelCol={{ span: 8 }}
+                            wrapperCol={{ span: 16 }}
+                            initialValues={{
+                                ...initialValues,
+                                remember: false,
+                            }}
+                            autoComplete="off"
+                        >
+                         {this.props.data?.virtualEnvServices
+                             .sort((a, b) => a.service_name > b.service_name ? 1 : -1)
+                             .map((service) => <ServiceInput service={service}/>)}
+                        </Form>
+                    </Layout>
                 </Modal>
             </div>
         );
---
+++ client/src/components/ServiceInput.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Form, Input } from 'antd';
+
+function ServiceInput({ service }) {
+    // console.log(service);
+    return (
+        <Form.Item
+            label={service.service_name}
+            name={service.id}
+            rules={[{ required: false, message: 'default' }]}
+        >
+            <Input placeholder="default tag" />
+        </Form.Item>
+    )
+}
+
+export default ServiceInput;
