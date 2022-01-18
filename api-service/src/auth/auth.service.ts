990fa4f9ddff2fc765b9b5a15482d8f4c3b412dd
snowsem <russnowsem@ya.ru>
2022-01-18 13:49:42 +0300

    auth

--- api-service/src/auth/auth.service.ts
+++ api-service/src/auth/auth.service.ts
@@ -20,42 +20,42 @@ export class AuthService {
     }
 
     authenticateUserByGoogle = async ()=>{
-        const googleClient = new OAuth2Client({
-            clientId: `${process.env.GOOGLE_CLIENT_ID}`,
-        });
-
-        const { token } = req.body;
-
-        const ticket = await googleClient.verifyIdToken({
-            idToken: token,
-            audience: `${process.env.GOOGLE_CLIENT_ID}`,
-            audient: `${process.env.GOOGLE_CLIENT_ID}`,
-        });
-
-        const userRepo = getRepository(User);
-
-        const payload = ticket.getPayload();
-
-        let user = await User.findOne({ email: payload?.email });
-
-        const _token = jwt.sign(
-            { user_id: user.id, user_email: user.email, token_google: token },
-            process.env.TOKEN_KEY,
-            {
-                expiresIn: "30d",
-            }
-        );
-
-        if (!user) {
-            user = new User()
-            user.email = payload?.email
-            user.avatar = payload?.picture
-            user.first_name = payload?.name
-            user.token_google = token
-            user.token = _token;
-
-            await userRepo.save(user);
-        }
+        // const googleClient = new OAuth2Client({
+        //     clientId: `${process.env.GOOGLE_CLIENT_ID}`,
+        // });
+        //
+        // const { token } = req.body;
+        //
+        // const ticket = await googleClient.verifyIdToken({
+        //     idToken: token,
+        //     audience: `${process.env.GOOGLE_CLIENT_ID}`,
+        //     audient: `${process.env.GOOGLE_CLIENT_ID}`,
+        // });
+        //
+        // const userRepo = getRepository(User);
+        //
+        // const payload = ticket.getPayload();
+        //
+        // let user = await User.findOne({ email: payload?.email });
+        //
+        // const _token = jwt.sign(
+        //     { user_id: user.id, user_email: user.email, token_google: token },
+        //     process.env.TOKEN_KEY,
+        //     {
+        //         expiresIn: "30d",
+        //     }
+        // );
+        //
+        // if (!user) {
+        //     user = new User()
+        //     user.email = payload?.email
+        //     user.avatar = payload?.picture
+        //     user.first_name = payload?.name
+        //     user.token_google = token
+        //     user.token = _token;
+        //
+        //     await userRepo.save(user);
+        // }
     }
 
     me = async ()=>{
